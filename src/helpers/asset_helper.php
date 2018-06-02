fd0b5f4b5819e5a9b3c497665aa4bf009dc03bb7
roliod <rolandoduberu@gmail.com>
2018-06-02 23:43:56 +0100

    author sign

--- src/helpers/asset_helper.php
+++ src/helpers/asset_helper.php
@@ -1,5 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+* Codeigniter asset versioning installer
+*
+* @author     Roland Oduberu <https://github.com/roliod>
+* @license    MIT License
+* @copyright  2018 Roland Oduberu
+* @link       https://github.com/roliod/codeigniter-asset-versioning/releases
+*/
+
 if (! function_exists('version_url')) {
 
     function version_url($path = '')
--- src/libraries/asset.php
+++ src/libraries/asset.php
@@ -1,5 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+* Codeigniter asset versioning installer
+*
+* @author     Roland Oduberu <https://github.com/roliod>
+* @license    MIT License
+* @copyright  2018 Roland Oduberu
+* @link       https://github.com/roliod/codeigniter-asset-versioning/releases
+*/
+
 class asset extends CI_Model
 {
 
@@ -10,5 +19,5 @@ class asset extends CI_Model
 
         return $base_url . '/' . $path . '?v=' . $version;
      }
-     
+
 }
