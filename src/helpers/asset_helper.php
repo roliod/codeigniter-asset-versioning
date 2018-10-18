ba955d74e521290ed1fee0e8e2b237347dbe8880
roliod <rolandoduberu@gmail.com>
2018-10-18 11:34:26 +0100

    follow PSR-2

--- src/helpers/asset_helper.php
+++ src/helpers/asset_helper.php
@@ -1,22 +1,22 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
 /**
-* Codeigniter asset versioning installer
-*
-* @author     Roland Oduberu <https://github.com/roliod>
-* @license    MIT License
-* @copyright  2018 Roland Oduberu
-* @link       https://github.com/roliod/codeigniter-asset-versioning/releases
-*/
+ * Codeigniter asset versioning installer
+ *
+ * @author     Roland Oduberu <https://github.com/roliod>
+ * @license    MIT License
+ * @copyright  2018 Roland Oduberu
+ * @link       https://github.com/roliod/codeigniter-asset-versioning/releases
+ */
 
-if (! function_exists('version_url')) {
+if (!function_exists('version_url')) {
+    function version_url($path = '')
+    {
+        $CI = &get_instance();
+        $CI->load->library('asset');
 
-  function version_url($path = '')
-  {
-    $CI =& get_instance();
-    $CI->load->library('asset');
-
-    return $CI->asset->version_url($path);
-  }
-
+        return $CI->asset->version_url($path);
+    }
 }
--- src/libraries/asset.php
+++ src/libraries/asset.php
@@ -1,23 +1,23 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
 /**
-* Codeigniter asset versioning installer
-*
-* @author     Roland Oduberu <https://github.com/roliod>
-* @license    MIT License
-* @copyright  2018 Roland Oduberu
-* @link       https://github.com/roliod/codeigniter-asset-versioning/releases
-*/
+ * Codeigniter asset versioning installer
+ *
+ * @author     Roland Oduberu <https://github.com/roliod>
+ * @license    MIT License
+ * @copyright  2018 Roland Oduberu
+ * @link       https://github.com/roliod/codeigniter-asset-versioning/releases
+ */
 
 class asset extends CI_Model
 {
+    public function version_url($path = '')
+    {
+        $base_url = $this->config->item('base_url');
+        $version = filemtime(FCPATH . $path);
 
-  public function version_url($path = '')
-  {
-    $base_url = $this->config->item('base_url');
-    $version = filemtime(FCPATH . $path);
-
-    return $base_url . '/' . $path . '?v=' . $version;
-  }
-
+        return $base_url . '/' . $path . '?v=' . $version;
+    }
 }
